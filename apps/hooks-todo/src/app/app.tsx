ea48c91432155675c03ee620a50467044277449c
Timothy Curchod <timofeyc@hotmail.com>
2019-04-27 20:13:01 +1000

    Finished crud functions in hooks

--- apps/hooks-todo/src/app/app.tsx
+++ apps/hooks-todo/src/app/app.tsx
@@ -1,8 +1,21 @@
 import React, { useState } from "react";
 import './app.scss';
-const Todo = ({ todo }) => <div className="todo">{todo.text}</div>;
+// create a component that we can use in the return of the main App component
+function Todo({ todo, index, completeTodo, removeTodo }) {
+  return (
+    <div className="todo"
+      style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}>
+      {todo.text}
+      <div>
+        <button onClick={() => completeTodo(index)}>Complete</button>
+        <button class="removeTodo" onClick={() => removeTodo(index)}>X</button>
+      </div>
+    </div>
+  );
+}
 
 function TodoForm({ addTodo }) {
+  // Start with an empty state
   const [value, setValue] = useState("");
   const handleSubmit = e => { e.preventDefault();
     if (!value) return;
@@ -20,23 +33,39 @@ function TodoForm({ addTodo }) {
 
 function App() {
   const [todos, setTodos] = useState([
-    { text: "Learn about React" },
-    { text: "Meet friend for lunch" },
-    { text: "Build really cool todo app" }
-  ]);
+     { text: "Learn about React",
+       isCompleted: false },
+     { text: "Meet friend for lunch",
+       isCompleted: false },
+     { text: "Build really cool todo app",
+       isCompleted: false }]);
   const addTodo = text => {
+    // this.state is implied in certain places.
     const newTodos = [...todos, { text }];
     setTodos(newTodos);
-
+  };
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = !newTodos[index].isCompleted;
+    setTodos(newTodos);
+  };
+  const removeTodo = index => {
+   const newTodos = [...todos];
+   newTodos.splice(index, 1);
+   setTodos(newTodos);
   };
   return (
     <div className="app">
       <div className="todo-list">
+        {/* create a new array of items by mapping over the
+         todo items from state and displaying them by index. */}
         {todos.map((todo, index) => (
           <Todo
             key={index}
             index={index}
             todo={todo}
+            completeTodo={completeTodo}
+            removeTodo={removeTodo}
           />
         ))}
         <TodoForm addTodo={addTodo} />
