894fc0aaaa0d61ed42781b53360f4c7403f571ac
Timothy Curchod <timofeyc@hotmail.com>
2019-08-25 17:19:58 +1000

    #1 step two fail

--- apps/sunday/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'sunday',
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/apps/sunday/',
-  snapshotSerializers: [
-    'jest-preset-angular/AngularSnapshotSerializer.js',
-    'jest-preset-angular/HTMLCommentSerializer.js'
-  ]
-};
--- apps/sunday/src/app/app.module.ts
+++ apps/sunday/src/app/app.module.ts
@@ -1,16 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabled' })
+  entryComponents: [],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
